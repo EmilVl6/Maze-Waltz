bc27ec7e7a86b9e6361ce7e44203f1678250880f
emilv <93434155+EmilVl6@users.noreply.github.com>
2025-11-05 15:45:47 +1100

    Reworked Waltz

--- Maze_Waltz/Maze_Waltz.ino
+++ Maze_Waltz/Maze_Waltz.ino
@@ -34,15 +34,21 @@ void setup() {
 #define TurnL 2
 #define TurnR 3
 #define DeadEnd 4
+#define LongDeadEnd 5
 
 #define timeExit 100
 #define timeTurn 1700
+#define timeForward 800
 
 #define moveFwd 100
 #define veer 30
 #define turn 40
 
-int valLeft = 0, valRight = 0, valMiddle = 0; int MotionState = Fwd; bool deadEnd = 1;
+int valLeft = 0, valRight = 0, valMiddle = 0; 
+int MotionState = Fwd; 
+bool deadEnd = 1;
+unsigned long forwardStartTime = 0;
+bool inStraightSection = false;
 
 void loop() {
   valLeft = irDistance(L_irLedPin, L_irReceiverPin, L_redLedPin);
@@ -55,36 +61,74 @@ void loop() {
   switch(MotionState) {
     case Fwd: {
       Serial.println("Fwd");
-      if(valLeft == valRight) {
-        Serial.println("Straight");
+      
+      if(valRight >= distMax && valMiddle >= distMax) {
         move(moveFwd, 0);
-      }
-      if(valLeft > valRight) {
-        Serial.println("Veer Left");
-        move(moveFwd - veer/2, -veer/2);
-      }
-      if(valRight > valLeft) {
-        Serial.println("Veer Right");
-        move(moveFwd - veer/2, veer/2);
-      }
-      if(valLeft>=distMax && valMiddle<distMax) {
-        MotionState = TurnL;
-        break;
-      }
-      if(valRight>=distMax && valMiddle<distMax) {
+        delay(timeExit*4);
         MotionState = TurnR;
+        //delay(10);
         break;
       }
-      if(valLeft<distMax && valRight<distMax && valMiddle<distMax-2) {
-        MotionState = DeadEnd;
+      else if(valRight >= distMax && valMiddle < distMax) {
+        move(moveFwd, 0);
+        delay(timeExit*2);
+        MotionState = TurnR;
+        //delay(10);
         break;
+      }
+      else if(valMiddle < distMax) {
+        if(valLeft >= distMax) {
+          move(moveFwd, 0);
+          delay(timeExit*2);
+          MotionState = TurnL;
+          //delay(10);
+        } else {
+          MotionState = DeadEnd;
+          //delay(10);
+        }
+        break;
+      }
+      else {
+        if(valRight < distMax && valLeft < distMax) {
+          Serial.println("Corridor - balanced");
+          if(valLeft == valRight) {
+            move(moveFwd, 0);
+          }
+          else if(valLeft > valRight) {
+            move(moveFwd - veer/4, -veer/4);
+          }
+          else {
+            move(moveFwd - veer/4, veer/4);
+          }
+        }
+        else if(valRight < distMax) {
+          Serial.println("Following right wall");
+          move(moveFwd - veer/3, -veer/3);
+        }
+        else if(valLeft < distMax) {
+          Serial.println("Following left wall");
+          move(moveFwd - veer/3, veer/3);
+        }
+        else {
+          Serial.println("Open area");
+          move(moveFwd, 0);
+        }
+        
+        if(valLeft < distMax && valRight < distMax && valMiddle >= distMax) {
+          if(!inStraightSection) {
+            inStraightSection = true;
+            forwardStartTime = millis();
+          }
+        } else {
+          inStraightSection = false;
+        }
       }
     break;
     }
     case TurnL: {
       Serial.println("TurnL");
-      move(moveFwd/2, 0);
-      delay(timeExit*5);
+      move(moveFwd, 0);
+      delay(timeExit*3);
       move(turn/2, -turn/2);      
       delay(timeTurn*1.2);
       move(moveFwd, 0);
@@ -94,8 +138,6 @@ void loop() {
     }
     case TurnR: {
       Serial.println("TurnR");
-      move(moveFwd/2, 0);
-      delay(timeExit*5);
       move(turn/2, turn/2);      
       delay(timeTurn*1.1);
       move(moveFwd, 0);
@@ -104,19 +146,30 @@ void loop() {
     break;
     }
     case DeadEnd: {
-      if(deadEnd) {
-        Serial.println("DeadEnd");
-        move(0, -turn);      
+      bool isLongDeadEnd = inStraightSection && (millis() - forwardStartTime > timeForward);
+      
+      if(isLongDeadEnd && deadEnd) {
+        Serial.println("Long Dead End - FINISH!");
+        MotionState = LongDeadEnd;
+      }
+      else if(deadEnd) {
+        Serial.println("Short Dead End - Turn Around");
+        move(-turn/2, -turn);
         delay(timeTurn*1.4);
         move(moveFwd, 0);
         delay(timeExit);
-        MotionState=Fwd;
+        MotionState = Fwd;
         deadEnd = 0;
       }
       else {
-        Serial.println("End");
-        move(0, 0);      
+        Serial.println("Multiple dead ends - stopping");
+        move(0, 0);
       }
+    break;
+    }
+    case LongDeadEnd: {
+      Serial.println("FINISH AREA REACHED!");
+      move(0, 0);
     break;
     }    
   }
